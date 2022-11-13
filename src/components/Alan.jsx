9c7af23c67fa453b4f952c02db901721a609cc73
bernard <bernardansah5@gmail.com>
2022-11-13 09:01:51 +0000

    Alan ai, featured movie card, minor fixes

---
+++ src/components/Alan.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useContext } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+import { ColorModeContext } from '../utils/ToggleColorMode';
+import { fetchToken } from '../utils/index';
+import { selectGenreOrCategory, searchMovie } from '../features/currentGenreOrCategory';
+
+const useAlan = () => {
+  const { setMode } = useContext(ColorModeContext);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  useEffect(() => {
+    alanBtn({
+      key: '2e7428c219905b78ee67f5155a84dcca2e956eca572e1d8b807a3e2338fdd0dc/stage',
+      onCommand: ({ command, mode, genres, genreOrCategory, query }) => {
+        if (command === 'chooseGenre') {
+          const foundGenre = genres.find((g) => g.name.toLowerCase() === genreOrCategory.toLowerCase());
+          if (foundGenre) {
+            navigate('/');
+            dispatch(selectGenreOrCategory(foundGenre.id));
+          } else {
+            const category = genreOrCategory.startsWith('top') ? 'top_rated' : genreOrCategory;
+            navigate('/');
+            dispatch(selectGenreOrCategory(category));
+          }
+        } else if (command === 'changeMode') {
+          if (mode === 'light') {
+            setMode('light');
+          } else {
+            setMode('dark');
+          }
+        } else if (command === 'login') {
+          fetchToken();
+        } else if (command === 'logout') {
+          localStorage.clear();
+          navigate('/');
+        } else if (command === 'search') {
+          dispatch(searchMovie(query));
+        }
+      },
+    });
+  }, []);
+};
+
+export default useAlan;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { CssBaseline } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
 
 import useStyles from './styles';
+import useAlan from './Alan';
 import { Actors, MovieInformation, Movies, NavBar, Profile } from '.';
 
 const App = () => {
   const classes = useStyles();
+  const alanBtnContainer = useRef();
+
+  useAlan();
   return (
     <div className={classes.root}>
       <CssBaseline />
@@ -21,6 +25,7 @@ const App = () => {
           <Route exact path="/profile/:id" element={<Profile />} />
         </Routes>
       </main>
+      <div ref={alanBtnContainer} />
     </div>
   );
 };
